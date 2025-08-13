ea27df72c8f5a2c14e9705ce61ad52371929e381
Walter Medvedeo <wmedvedeo@gmail.com>
2025-08-13 15:27:56 +0200

    Create the osl-1.36.0 branch and adjust the example to execute in that version

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -5,10 +5,10 @@
 REGISTRY_USER=$USER
 REGISTRY=quay.io
 
-IMG=$REGISTRY/$USER/serverless-workflow-kafka-with-tls:1.0-1.35.0-00
+IMG=$REGISTRY/$USER/serverless-workflow-kafka-with-tls:1.0-1.36.0-00
 
 # additional extensions to connect with kafka.
-QUARKUS_EXTENSIONS=io.quarkus:quarkus-smallrye-reactive-messaging-kafka:3.8.6.redhat-00004
+QUARKUS_EXTENSIONS=io.quarkus:quarkus-smallrye-reactive-messaging-kafka:3.15.4.redhat-00001
 
 docker build -f ./docker/Dockerfile \
   --build-arg=QUARKUS_EXTENSIONS=$QUARKUS_EXTENSIONS \
